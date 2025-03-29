7d0e1d632364e5422e3d2a53a5884468a90bf7ba
odinabkh1721 <bahodirovnaodina12@gmail.com>
2025-03-30 00:46:38 +0500

    Add files via upload

---
+++ SQLQuery4.sql
@@ -0,0 +1,64 @@
+1) AnSwer
+DATA  --malumot
+ 
+ DATABASE --malumotlari bazasi
+
+ RELATIONAL DATABASE --Aloqador ma'lumotlar bazasi 
+
+ TABLE  --Jadval
+
+ 2)Answer
+ FIVE KEY FEATURES OF SQL Server 
+
+ 1)High Performance Scalability -SQL Server 
+ optimizes query execution,supports indec=xing, 
+ and provides in memory,
+
+2)Security Compliances --It offers robust security features like
+Transparent Data Encryptionn (TDE)
+
+3)Backup Recovey  --SQL Server proides automatic backup
+options point in time recovery and high availability through features 
+like Always On Availability Groups
+
+4)Advanced Data Integrationn with built in tools like SQL Server
+Integration Services (SSIS)and PolyBase.
+
+5)Comprehensive BI Analytics Support SQL Server includes SQL Server 
+Analysis Services (SSAS) and SQL Server Reporting Services (SSRS)
+
+
+3 Answer
+
+1.Windows Authentication 
+2.SQL Server Authentication
+
+7)Answer
+--DQL --data query language 
+
+--DML --data manipulation language 
+
+--DDL --data definition language 
+
+--DCL --data control language 
+
+--TCL  --Transaction control language 
+
+
+8)Answer 
+
+INSERT INTO Student (StudentID,NAME,AGE Values 
+(1,'Ali',14,'84')
+(2,'Madina',15,'9B')
+(3,'Nigora',13,'7A')
+
+
+9)Answer
+
+BACKUP DATABASE School1DB
+to DISK = 'c:\backup\SchoolDB.bak';
+
+9)Restore DATABASE SchoolDB
+FROM DISK = 'C:\backup\SchoolDB.bak';
+
+
